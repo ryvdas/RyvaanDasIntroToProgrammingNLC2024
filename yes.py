45f4c68ea67b62d62e00509f1dbb768cedb2cf77
Ryvaan Das <ryvdas@gmail.com>
2024-06-25 12:14:21 -0400

    das

--- yes.py
+++ yes.py
@@ -80,6 +80,7 @@ with tab2:
             classData_df = classData_df.drop(i)
             #classData = st.dataframe(data=classData_df, hide_index=True)
             classData_df.to_csv('classData.csv', index = False)
+            classData = st.dataframe(data=classData_df, hide_index=True)
 
     if deleteButton:
         classOptions = []
